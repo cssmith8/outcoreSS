3e61cb5c9852dc7f5c8d003a633901ae12dafb57
Cohen Smith <cohens@mail.smu.edu>
2023-10-08 18:14:10 -0500

    copy into saves dir

--- main.cpp
+++ main.cpp
@@ -3,20 +3,37 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
+#include <windows.h>
 
 using namespace std;
+
+bool createSave(string appPath, string savesPath, string name) {
+    CopyFile((appPath + "\\Outcore\\save.me").c_str(), (savesPath + "\\" + name + ".s.me").c_str(), FALSE);
+    CopyFile((appPath + "\\Idle Game\\free.me").c_str(), (savesPath + "\\" + name + ".f.me").c_str(), FALSE);
+    return true;
+}
+
+bool deleteSave(string savesPath, string name) {
+    remove((savesPath + "\\" + name + ".s.me").c_str());
+    remove((savesPath + "\\" + name + ".f.me").c_str());
+    return true;
+}
 
 int main() {
 
     //navigate to appdata folder
-    string path = getenv("APPDATA");
-    path.erase(path.end()-8, path.end());
-    path.append("\\LocalLow\\DoctorShinobi\\Outcore\\save.me");
+    string appPath = getenv("APPDATA");
+    appPath.erase(appPath.end()-8, appPath.end());
+    appPath.append("\\LocalLow\\DoctorShinobi");
 
-    //convert path to char array
-    char *cpath = new char[path.length() + 1];
-    strcpy(cpath, path.c_str());
-    remove(cpath);
+    //save path to saves folder
+    string savesPath = "..\\saves";
+
+
+    //remove(appPath.c_str());
+
+    createSave(appPath, savesPath, "test");
+    deleteSave(savesPath, "test");
 
     return 0;
 }
